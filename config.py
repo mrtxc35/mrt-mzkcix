7ac958ae348f248bcebe5947c091d6e61372aaf4
ErdemBey01 <111714603+ErdemBey01@users.noreply.github.com>
2022-12-02 18:30:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    BOT_TOKEN = "5505282726:AAFthxAPwZNKBMKq1Touf0bwbTcG1v5Vp6I",
+    API_ID = "14993791",
+    API_HASH = "60a2897e2d7de8eefd6cf610085b1f0a",
+    BOT_OWNER = "1720623907"
