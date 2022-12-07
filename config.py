79b397c8696181d3080973fbac88fed99444a0ef
mrtxc35 <119897267+mrtxc35@users.noreply.github.com>
2022-12-08 00:29:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     BOT_TOKEN = "5644947301:AAHHRRTc6JeuvcizG4p24qxlkziwD01SYbw",
-    API_ID = "11385233",
-    API_HASH = "16d51f2c856dec1c9abf7f4b31fb9d6e",
+    API_ID = "19169979",
+    API_HASH = "2bb8a8a620e6cf1e44a1e9987230e95b",
     BOT_OWNER = "5333072972"
