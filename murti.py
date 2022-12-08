688686991c6f3a9b9ee04e5cc9f9f1a1068c36e1
mrtxc35 <119897267+mrtxc35@users.noreply.github.com>
2022-12-08 22:15:31 +0300

    Update murti.py

--- murti.py
+++ murti.py
@@ -17,7 +17,7 @@ bot = Client(
     'moonBot',
     bot_token = "5644947301:AAHHRRTc6JeuvcizG4p24qxlkziwD01SYbw",
     api_id = "11385233",
-    api_hash = "16d51f2c856dec1c9abf7f4b31fb9d6e"
+    api_hash = "16d51f2c856dec1c9abf7f4b31fb9d6e",
 )
 
 #start mesajı
