9ae8dff3d9b0d6ff96422a2061e7daccac95e690
mrtxc35 <119897267+mrtxc35@users.noreply.github.com>
2022-12-08 22:14:05 +0300

    Update murti.py

--- murti.py
+++ murti.py
@@ -15,9 +15,9 @@ from pyrogram.types import (
 
 bot = Client(
     'moonBot',
-    bot_token = 5644947301:AAHHRRTc6JeuvcizG4p24qxlkziwD01SYbw,
-    api_id = 11385233,
-    api_hash = 16d51f2c856dec1c9abf7f4b31fb9d6e
+    bot_token = "5644947301:AAHHRRTc6JeuvcizG4p24qxlkziwD01SYbw",
+    api_id = "11385233",
+    api_hash = "16d51f2c856dec1c9abf7f4b31fb9d6e"
 )
 
 #start mesajı
