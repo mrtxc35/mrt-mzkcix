017f01c1b64061f367df0b31bc435b6e204e2789
mrtxc35 <119897267+mrtxc35@users.noreply.github.com>
2022-12-14 20:42:08 +0300

    Update murti.py

--- murti.py
+++ murti.py
@@ -93,7 +93,7 @@ def bul(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**╠══════════════════╣\n╠   [➕ BOTU GRUBA EKLE ➕](https://t.me/muzik_indiren_bot?startgroup=a)\n╠══════════════════╣\n╠➥[🎵 Mp3 İndiren Bot](https://t.me/muzik_indiren_bot)\n╠➥[🎶 Çalma Listesi](https://t.me/PlayListMp3Murti)\n╠➥[🤖 Diğer Botlar](https://t.me/MurtiBots)**\n**İsim: thumb_name**\n╠══════════════════╣**"
+        rep = f"**╠══════════════════╣\n╠   [➕ BOTU GRUBA EKLE ➕](https://t.me/muzik_indiren_bot?startgroup=a)\n╠══════════════════╣\n╠➥[🎵 Mp3 İndiren Bot](https://t.me/muzik_indiren_bot)\n╠➥[🎶 Çalma Listesi](https://t.me/PlayListMp3Murti)\n╠➥[🤖 Diğer Botlar](https://t.me/MurtiBots)\n╠══════════════════╣**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
